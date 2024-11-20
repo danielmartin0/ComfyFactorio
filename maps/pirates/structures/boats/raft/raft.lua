74ceb822a21142511d944d0154d02db0856687de
danielmartin0 <github@danielgmartin.com>
2024-11-20 21:24:13 +0000

    v1 of hostable server version

--- maps/pirates/structures/boats/raft/raft.lua
+++
@@ -1,8 +0,0 @@
--- This file is part of thesixthroc's Pirate Ship softmod, licensed under GPLv3 and stored at https://github.com/ComfyFactory/ComfyFactorio and https://github.com/danielmartin0/ComfyFactorio-Pirates.
-
--- local Data = require 'maps.pirates.structures.boats.raft.data'
-
-local Public = {}
-Public.Data = require('maps.pirates.structures.boats.raft.data')
-
-return Public
